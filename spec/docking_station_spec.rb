e483485dd3bbe0edcaf16e0f1ec9f2cb26bae905
Charlotte Fereday <cbrfereday@gmail.com>
2017-05-09 15:09:31 +0100

    Updates spec with one line syntax

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,5 @@
 require 'docking_station'
 
 describe DockingStation do
-  # it "release bike when called on a docking station instance" do
-  #   dock = DockingStation.new
-  #   dock.release_bike
-  # end
-  subject { dock = DockingStation.new }
-  it { subject.release_bike }
+  it { is_expected.to respond_to :release_bike}
 end
