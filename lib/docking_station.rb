e2c2c50e698aca9549a61cb91a22de07d63f4b8e
Panteha <panteha.ahmadi@gmail.com>
2017-05-10 16:26:51 +0100

    Increase docking station size to 20 bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,14 +3,20 @@ require './lib/bike.rb'
 class DockingStation
   attr_reader :bike
 
+  def initialize
+    @bikes = []
+  end
+
+
   def release_bike
-    fail "No bikes available" unless @bike
-    @bike
+    fail "No bikes available" unless @bikes.empty?
+    @bike.pop
   end
 
   def dock(bike)
-    fail "Docking station full" if @bike
-    @bike = bike
+    puts bike
+    fail "Docking station full" if @bikes.count >= 20
+    @bikes << bike
   end
 
 
