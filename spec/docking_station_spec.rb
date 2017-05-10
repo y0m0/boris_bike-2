01c5e78bc68c276d7090fb4052b4255f29f9cf79
y0m0 <lixi.daniele@gmail.com>
2017-05-10 14:48:19 +0100

    test return docked bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,22 +3,23 @@ require 'docking_station'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
-
-
   it "releases working bikes" do
     bike = subject.release_bike
     expect(bike).to be_working
-  end
-
-  it "docks a bike" do
-    bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
   end
 
   it {is_expected.to respond_to(:dock).with(1).argument }
 
   it { is_expected.to respond_to :bike }
 
+  it "docks a bike" do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
 
-
+  it "returns docked bikes" do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
 end
