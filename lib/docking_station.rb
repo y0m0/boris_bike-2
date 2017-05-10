e1fe638e1fecad66d8232ea18aa9f359a54a51c7
Panteha <panteha.ahmadi@gmail.com>
2017-05-10 16:44:27 +0100

    Refactor release method

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -7,12 +7,16 @@ class DockingStation
     @bikes = []
   end
 
+  def empty?
+    @bikes.empty?
+  end
+
   def full?
     @bikes.count >= 20
   end
 
   def release_bike
-    fail "No bikes available" if @bikes.empty?
+    fail "No bikes available" if empty?
     @bikes.pop
   end
 
