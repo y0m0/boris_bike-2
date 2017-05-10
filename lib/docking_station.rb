f6692b13b95e28549bac98b959383e70fc6b8976
Panteha <panteha.ahmadi@gmail.com>
2017-05-10 12:36:40 +0100

    test dock bike

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,13 @@
 require './lib/bike.rb'
 
 class DockingStation
+
   def release_bike
     Bike.new
   end
+
+  def dock(bike)
+
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,8 +3,16 @@ require 'docking_station'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
+
+
   it "releases working bikes" do
     bike = subject.release_bike
     expect(bike).to be_working
   end
+
+    it {is_expected.to respond_to(:dock).with(1).argument }
+
+
+
+
 end
